e8389fc2fc1c320f52ff2792c1d0fc9ffd0f9878
Ndeye <ndeyependa@gmail.com>
2021-12-13 17:10:21 +0100

    first commit

---
+++ excercie.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/CSS/bootstrap.min.css">
+    <title>Les exercices</title>
+</head>
+<body>
+<?php include("./partiel/_navBar.php");?>
+<div class="container">
+<h1>Page des exo</h1>
+</div>
+   
+<script src="/JS/bootstrap.bundle.min.js"></script>    
+</body>
+</html>
